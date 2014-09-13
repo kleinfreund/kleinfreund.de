024bb7ff535504ab49d8be770d9216620efb2eda
kleinfreund <phrudloff@gmail.com>
2014-09-13 15:16:29 +0200

    Adding new gulpfile.js and packages.json

--- gulpfile.js
+++ gulpfile.js
@@ -7,49 +7,20 @@ var rename    = require('gulp-rename');
 
 
 
-// Options
-var options = {};
-
-options.sass = {
-    errLogToConsole: true,
-    sourceMap: 'sass',
-    sourceComments: 'map'
-};
-
-options.autoprefixer = {
-    map: true,
-    from: 'css',
-    to: 'kleinfreund.min.css'
-};
-
-
-
-// work around for sourcemaps: https://github.com/dlmanning/gulp-sass/issues/28#issuecomment-43951089
-var processWinPath = function(file) {
-    var path = require('path');
-    if (process.platform === 'win32') {
-        file.path = path.relative('.', file.path);
-        file.path = file.path.replace(/\\/g, '/');
-    }
-};
-
-
-
 // Process Sass (compile, prefix, minify)
 gulp.task('sass', function() {
     return gulp.src('sass/**/*.scss')
-        // work around for sourcemaps: https://github.com/dlmanning/gulp-sass/issues/28#issuecomment-43951089
-        .on('data', processWinPath)
 
         // Process Sass files
-        .pipe(sass(options.sass))
+        .pipe(sass({
+            errLogToConsole: true
+        }))
 
         // Prefix CSS properties
-        // Autoprefixer doesn't play well with source maps
-        .pipe(prefix(
-            'last 2 version', '> 1%', 'ie 9', 'ie 8',
-            options.autoprefixer
-        ))
+        .pipe(prefix({
+            browsers: ['last 2 version', '> 1%', 'ie 9', 'ie 8'],
+            cascade: true
+        }))
 
         // Output regular `*.css`
         .pipe(gulp.dest('css'))
