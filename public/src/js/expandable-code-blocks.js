cc2edcf7e23bdfb5c614ea006900c6b239b38eea
kleinfreund <phrudloff@gmail.com>
2017-10-16 10:36:10 +0200

    Whoopsie! Remove Hugo output directory from git

--- public/src/js/expandable-code-blocks.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const codeBlocks = Array.from(document.querySelectorAll('pre'));
-  codeBlocks.forEach(block => {
-    if (block.scrollWidth > block.clientWidth) {
-      block.classList.add('expandable');
-    }
-  });
-});
