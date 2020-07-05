59ddbaa961a2ba033cc646ab7c83685251026429
kleinfreund <philipprudloff@fastmail.com>
2020-07-05 17:26:44 +0200

    chore: tweaking the styles a bit

--- src/js/color-scheme-toggle.js
+++ src/js/color-scheme-toggle.js
@@ -1,34 +1,27 @@
 (function () {
-  document.addEventListener('DOMContentLoaded', function () {
-    document.body.classList.remove('js-disabled');
-
+  document.addEventListener("DOMContentLoaded", function () {
+    document.body.classList.remove("js-disabled");
     initColorSchemeToggle();
   });
 })();
 
 function initColorSchemeToggle() {
-  const storedColorScheme = window.localStorage.getItem('active-color-scheme');
+  const storedColorScheme = window.localStorage.getItem("active-color-scheme");
   if (storedColorScheme !== null) {
-    document.documentElement.setAttribute('data-color-scheme', storedColorScheme);
+    document.body.setAttribute("data-color-scheme", storedColorScheme);
   }
 
-  const toggle = document.querySelector('[data-color-scheme-toggle]');
-  toggle.addEventListener('click', function () {
-    toggleColorScheme();
-  });
+  const toggle = document.querySelector("[data-color-scheme-toggle]");
+  toggle.addEventListener("click", toggleColorScheme);
 }
 
 function toggleColorScheme() {
   const activeColorScheme = getActiveColorScheme();
-  if (activeColorScheme === 'light') {
-    setActiveColorScheme('dark');
-  } else {
-    setActiveColorScheme('light');
-  }
+  setActiveColorScheme(activeColorScheme === "light" ? "dark" : "light");
 }
 
 function getActiveColorScheme() {
-  const activeColorScheme = document.documentElement.getAttribute('data-color-scheme');
+  const activeColorScheme = document.body.getAttribute("data-color-scheme");
   if (activeColorScheme !== null) {
     return activeColorScheme;
   }
@@ -38,19 +31,19 @@ function getActiveColorScheme() {
     return colorSchemePreference;
   }
 
-  return 'light';
+  return "light";
 }
 
 function setActiveColorScheme(colorScheme) {
-  document.documentElement.setAttribute('data-color-scheme', colorScheme);
-  window.localStorage.setItem('active-color-scheme', colorScheme);
+  document.body.setAttribute("data-color-scheme", colorScheme);
+  window.localStorage.setItem("active-color-scheme", colorScheme);
 }
 
 function getColorSchemePreference() {
   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    return 'dark';
+    return "dark";
   } else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-    return 'light';
+    return "light";
   }
 
   return null;
