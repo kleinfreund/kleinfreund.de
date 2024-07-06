fd5014d1ecc2724423232eee575fc0823a11bbef
kleinfreund <5774638+kleinfreund@users.noreply.github.com>
2024-07-06 16:15:29 +0200

    Remove color scheme toggle altogether

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,6 @@
 // @ts-check
 
-init()
-
-function init() {
-  initColorSchemeToggle()
-  setCurrentEmploymentTime()
-}
-
-function initColorSchemeToggle() {
-  document.querySelector('[data-color-scheme-toggle]')
-    ?.addEventListener('click', function () {
-      const newActiveColorScheme = document.documentElement.getAttribute('data-color-scheme') === 'light' ? 'dark' : 'light'
-      document.documentElement.setAttribute('data-color-scheme', newActiveColorScheme)
-      window.localStorage.setItem('active-color-scheme', newActiveColorScheme)
-    })
-
-  // Initially set the `data-color-scheme` attribute to either (1) the stored color scheme or (2) the preferred color scheme. From this point on, the `data-color-scheme` is the only thing controlling the active scheme. This together with the `light-dark()` function in CSS removes the need for dealing with the `prefers-color-scheme` media query in CSS.
-  document.documentElement.setAttribute('data-color-scheme', getColorSchemePreference())
-}
-
-function getColorSchemePreference() {
-  const storedColorScheme = window.localStorage.getItem('active-color-scheme')
-  if (storedColorScheme !== null) {
-    return storedColorScheme
-  } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    return 'dark'
-  }
-
-  return 'light'
-}
+setCurrentEmploymentTime()
 
 function setCurrentEmploymentTime() {
   const elements = Array.from(document.querySelectorAll('[data-start-date]'))
