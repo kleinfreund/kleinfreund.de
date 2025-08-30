c2c76eaffee8ef3f11f42662c4596c3c6cbe552e
kleinfreund <5774638+kleinfreund@users.noreply.github.com>
2025-08-30 09:33:20 +0200

    Sort records alphabetically by artist name

--- discogs/discogs.js
+++ discogs/discogs.js
@@ -21,13 +21,24 @@ export default async function () {
     .filter((release) => release.basic_information.formats.some((format) => format.name === 'Vinyl'))
   console.log('[Discogs] Done.')
 
-  const records = releases.map(({ basic_information, date_added }) => ({
-    title: basic_information.title.trim(),
+  const records = releases.map(({ basic_information, date_added }) => {
+    const title = basic_information.title.trim()
     // Strip off the sequential number added to artist names if there are multiple in the Discogs database (e.g. “Air (10)” → “Air”).
-    artists: basic_information.artists.map(({ name }) => name.replace(/ \(\d+\)$/, '')),
-    dateAdded: new Date(date_added),
-  }))
-  records.sort((recordA, recordB) => recordB.dateAdded.getTime() - recordA.dateAdded.getTime())
+    const artists = basic_information.artists.map(({ name }) => name.replace(/ \(\d+\)$/, ''))
+    const year = basic_information.year
+    const dateAdded = new Date(date_added)
+
+    return { title, artists, year, dateAdded }
+  })
+
+  records.sort((recordA, recordB) => {
+    const result = recordA.artists[0].localeCompare(recordB.artists[0])
+    if (result !== 0) {
+      return result
+    }
+
+    return recordB.year - recordA.year
+  })
 
   return records
 }
